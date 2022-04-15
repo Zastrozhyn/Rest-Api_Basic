cf6ce100a415695adaf6debcbf6e3c552827e20e
Zastrozhyn <Alexandr_Zastrozhyn@epam.com>
2022-04-15 19:05:03 +0300

    mySql -> gradle, + Log4j

--- Service/src/main/java/com/epam/esm/exception/TagNotFoundException.java
+++ Service/src/main/java/com/epam/esm/exception/TagNotFoundException.java
@@ -3,6 +3,17 @@ package com.epam.esm.exception;
 public class TagNotFoundException extends RuntimeException{
     private final int ERROR_CODE = 40401;
 
+    public TagNotFoundException() {
+    }
+
+    public TagNotFoundException(String message) {
+        super(message);
+    }
+
+    public TagNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public int getErrorCode() {
         return ERROR_CODE;
     }
--- Web/src/main/java/com/epam/esm/controller/TagController.java
+++ Web/src/main/java/com/epam/esm/controller/TagController.java
@@ -2,12 +2,14 @@ package com.epam.esm.controller;
 
 import com.epam.esm.entity.Tag;
 import com.epam.esm.service.TagService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping("/tags")
 public class TagController {
